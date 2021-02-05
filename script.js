69092c5976f74c2f79ecc7218327b8a018b6b025
Tabiul-Ifty <tabiulifty@gmail.com>
2021-02-05 16:20:25 +0600

    done

--- script.js
+++ script.js
@@ -5,6 +5,59 @@ randomPin.addEventListener("click", function(event){
     let random = parseInt(floor);
     console.log(random);
     const GeneratedPinField = document.getElementById('form-control');
-   
     GeneratedPinField.value = random;
 })
+
+const digitBtn = document.getElementById('digits');
+digitBtn.addEventListener('click',function(event){
+    const digit = event.target.innerText;
+    const typedInput = document.getElementById('inputField');
+
+    if (isNaN(digit)) {
+        console.log("not digit")
+        if (digit === 'C'){
+            typedInput.value = "";
+        }
+        if (digit === 'B'){
+            const input = typedInput.value;
+            const newInput = input.slice(0, -1);
+            typedInput.value = newInput;
+        }
+    }
+    else {
+        console.log(digit);
+        typedInput.value = typedInput.value + digit;
+        
+    }
+})
+
+
+const matchingInput = document.getElementById('pinMatching');
+matchingInput.addEventListener("click", function(){
+    const typedInput = document.getElementById('inputField');
+    const GeneratedPinField = document.getElementById('form-control');
+    console.log(typedInput.value);
+    console.log(GeneratedPinField.value);
+
+    const pinNotMatched = document.getElementById('notMatched');
+    const pinMatched = document.getElementById('matched');
+
+    if (typedInput.value == GeneratedPinField.value)
+    {
+        console.log("Nihal");
+        pinNotMatched.style.display = "none";
+        pinMatched.style.display = "block";
+    }
+    else{
+        console.log("not nihal");
+        pinNotMatched.style.display = "block";
+        pinMatched.style.display = "none";
+    }
+
+})
+
+
+// 2
+// var str = "Hello TecAdmin!";
+// var newStr = str.slice(0, -1);
+// console.log(newStr);
